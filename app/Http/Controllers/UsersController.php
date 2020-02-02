f92f4e7658d3f6ece2f3395d8b7bae077273501a
Yan Hu <yan.hu@rightcapital.com>
2020-02-02 17:45:37 +0800

    Add user avatar upload function.

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Auth;
+use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
@@ -19,9 +20,19 @@ class UsersController extends Controller
         return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request, User $user)
+    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
-        $user->update($request->all());
+        $data = $request->all();
+
+        if (isset($request->avatar)) {
+            $result = $uploader->save($request->avatar, 'avatars', $user->id);
+
+            if(is_array($result) && isset($result['path'])) {
+                $data['avatar'] = $result['path'];
+            }
+        }
+
+        $user->update($data);
 
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
