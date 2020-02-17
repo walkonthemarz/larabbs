160f280082fda4cba30611d2e42b862458fe31a3
Yan Hu <yan.hu@rightcapital.com>
2020-02-17 23:24:40 +0800

    Add topics page.

--- public/js/app.js
+++ public/js/app.js
@@ -37071,8 +37071,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/rick/larabbs/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/rick/larabbs/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/kenny/php/larabbs/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/kenny/php/larabbs/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
